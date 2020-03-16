dda6604f7e580369f1e5ac45de727f89b9a1f0fb
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2020-03-16 16:04:04 -0400

    formatting

--- sql/report.sql
+++ sql/report.sql
@@ -1,22 +1,24 @@
-select 'How many:' AS '';
-select '  - users:' AS '';
-select count(*) from user;
-select '  - users with over 100 edits:' AS '';
-select count(*) from user where number_of_edits > 100;
-select '  - pages:' AS '';
-select count(*) from page;
-select '  - edits:' AS '';
-select count(*) from edit;
-select '' AS '';
-select 'Proportion of:' AS '';
+SELECT 'How many:' AS '';
+SELECT '  - users:' AS '';
+SELECT count(*) FROM user;
+SELECT '  - users with over 100 edits:' AS '';
+SELECT count(*) FROM user where number_of_edits > 100;
+SELECT '  - pages:' AS '';
+SELECT count(*) FROM page;
+SELECT '  - edits:' AS '';
+SELECT count(*) FROM edit;
+SELECT '' AS '';
+
+SELECT 'Proportion of:' AS '';
     /* - edits containing bad words */
-select '  - partitions that are done vs error:' AS '';
+SELECT '  - partitions that are done vs error:' AS '';
 SELECT status, count(id) FROM partition GROUP BY status; 
-select '  - pages with talk pages:' AS '';
+SELECT '  - pages with talk pages:' AS '';
 SELECT namespace, count(page_id) FROM page GROUP BY namespace; 
-select '' AS '';
-select 'Distribution of:' AS '';
-select '  - users by number of edits:' AS '';
+SELECT '' AS '';
+
+SELECT 'Distribution of:' AS '';
+SELECT '  - users by number of edits:' AS '';
 SELECT
     FLOOR( user.number_of_edits / stat.diff ) * stat.diff as range_start, 
     (FLOOR( user.number_of_edits / stat.diff ) +1) * stat.diff -1 as range_end, 
@@ -27,9 +29,11 @@ FROM user,
     FROM user
     ) AS stat
 GROUP BY FLOOR( user.number_of_edits / stat.diff );
-select '' AS '';
-select 'Size of database:' AS '';
-SELECT table_schema AS "Database", SUM(data_length + index_length) / 1024 / 1024 / 1024 AS "Size (GB)" FROM information_schema.TABLES GROUP BY table_schema;
+SELECT '' AS '';
+
+SELECT 'Size of database:' AS '';
+SELECT table_schema AS "Database", SUM(data_length + index_length) / 1024 / 1024 / 1024 AS "Size (GB)" 
+    FROM information_schema.TABLES GROUP BY table_schema;
 
 /* need to add that user also edits mainspace */
 /* maybe still keep number of edits? would be good for analysis */
